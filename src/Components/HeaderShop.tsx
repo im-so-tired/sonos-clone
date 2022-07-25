78d00676ca76fd81bdc4b0a4941c79b4f631437e
im-so-tired <markov-pt@mail.ru>
2022-07-25 14:52:49 +0300

    add adaptive-strike

--- src/Components/HeaderShop.tsx
+++ src/Components/HeaderShop.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { AiOutlineUser, AiOutlineSearch } from "react-icons/ai";
 import { BsCart4 } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { AppendModal } from "./modalWindow/appendProduct";
 export const HeaderShop: React.FC = () => {
+  const burgerClick = () => {
+    const hiddenMenu = document.getElementById("modal-menu");
+    hiddenMenu?.classList.toggle("hidden");
+  };
   return (
     <header>
       <div className="container-bg header-shop">
@@ -42,6 +47,27 @@ export const HeaderShop: React.FC = () => {
             </li>
           </ul>
         </div>
+        <div onClick={burgerClick} className="burger">
+          <GiHamburgerMenu></GiHamburgerMenu>
+        </div>
+      </div>
+      <div id="modal-menu" className="modal-menu hidden">
+        <ul className="nav flex-container">
+          <li className="flex-container">
+            <Link to="/shop">
+              <span>Shop</span>
+            </Link>
+          </li>
+          <li className="flex-container">
+            <span>Learn</span>
+          </li>
+          <li className="flex-container">
+            <span>Support</span>
+          </li>
+          <li className="flex-container">
+            <span>Radio</span>
+          </li>
+        </ul>
       </div>
     </header>
   );
